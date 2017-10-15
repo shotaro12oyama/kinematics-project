009e055588ad7540a59d71a7bec7c85f1c0b29f0
Shotaro Oyama <shotaro12oyama@gmail.com>
2017-10-16 03:42:24 +0900

    update

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -70,8 +70,8 @@ def IK_parameter(px, py, pz, roll, pitch, yaw):
     ROT_EE = ROT_EE * Rot_Error
     ROT_EE = ROT_EE.subs({'r': roll, 'p': pitch, 'y': yaw})
     EE = Matrix([[px],
-                    [py],
-                    [pz]])
+                 [py],
+                 [pz]])
     WC = EE - (0.303) * ROT_EE[:, 2]
 
     # Calculate joint angles using Geomatric IK method
@@ -97,7 +97,6 @@ def IK_parameter(px, py, pz, roll, pitch, yaw):
     theta5 = atan2(sqrt(R3_6[0,2] * R3_6[0,2] + R3_6[2,2] * R3_6[2,2]), R3_6[1,2])
     theta6 = atan2(-R3_6[1,1], R3_6[1,0])
     theta = [theta1, theta2, theta3, theta4, theta5, theta6]
-    print(theta)
     return theta
 
 
